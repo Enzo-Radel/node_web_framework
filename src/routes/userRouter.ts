e02a0a983ce2e038aa3b10160a4c042780dbe3a0
Enzo-Radel <enzo@radel.com.br>
2023-09-14 14:44:42 -0300

    feat: create all functions for user

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,4 +4,8 @@ import UserController from "../controllers/UserController";
 
 export const userRouter = Router();
 
-userRouter.get("/", UserController.test);
+userRouter.get("/", UserController.index);
+userRouter.get("/store", UserController.store);
+userRouter.get("/show", UserController.show);
+userRouter.get("/update", UserController.update);
+userRouter.get("/delete", UserController.delete);
